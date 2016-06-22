f0621c91b4dbb7337ad6196e4b9102db05d097d0
allwen <ArianGhasempour40@Gmail.com>
2016-06-22 11:16:13 +0430

    Create id.lua

---
+++ plugins/id.lua
@@ -0,0 +1,16 @@
+do
+function run(msg, matches)
+local reply_id = msg['id']
+local info = "¤Your #Name: "..msg.from.print_name.."\n¤Your #Username: @"..msg.from.username.."\n¤Your #ID: "..msg.from.id.."\n¤Your #Message: "..msg.text.."\n¤Your #phone: +"..msg.from.phone.."\n¤SuperGroup #Name: "..msg.to.print_name.."\n¤SuperGroup #ID: "..msg.to.id.."\n"
+reply_msg(reply_id, info, ok_cb, false)
+end
+
+return {
+patterns = {
+"^[!/#](id)$",
+"^([Ii]d)$"
+},
+run = run
+}
+
+end
