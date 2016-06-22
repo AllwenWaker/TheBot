e0b3f7c56f5b43dcbf334cbb0030171e4c557833
allwen <ArianGhasempour40@Gmail.com>
2016-06-22 11:39:15 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,8 +1,6 @@
 do
 function run(msg, matches)
-local reply_id = msg['id']
-local info = "~Your #Name: "..msg.from.print_name.."\n~Your #Username: @"..msg.from.username.."\n~Your #ID: "..msg.from.id.."\n~Your #Message: "..msg.text.."\n~Your #Phone: +"..msg.from.phone.."\n~SuperGroup #Name: "..msg.to.print_name.."\n~SuperGroup #Id: "..msg.to.id.."\n"
-reply_msg(reply_id, info, ok_cb, false)
+return "~Your #Name: "..msg.from.print_name.."\n~Your #Username: @"..msg.from.username.."\n~Your #ID: "..msg.from.id.."\n~Your #Message: "..msg.text.."\n~Your #Phone: +"..msg.from.phone.."\n~SuperGroup #Name: "..msg.to.print_name.."\n~SuperGroup #Id: "..msg.to.id.."\n"
 end
 
 return {
