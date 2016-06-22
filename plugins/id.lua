69532094ebdf351691baae674edfa284a6b469d1
allwen <ArianGhasempour40@Gmail.com>
2016-06-22 11:41:52 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,6 +1,5 @@
-do
-function run(msg, matches)
-return "~Your #Name: "..msg.from.print_name.."\n~Your #Username: @"..msg.from.username.."\n~Your #ID: "..msg.from.id.."\n~Your #Message: "..msg.text.."\n~Your #Phone: +"..msg.from.phone.."\n~SuperGroup #Name: "..msg.to.print_name.."\n~SuperGroup #Id: "..msg.to.id.."\n"
+local function run(msg, matches)
+return "~Your #Name: "..msg.from.print_name.."\n~Your #Username: @"..msg.from.username.."\n~Your #ID: "..msg.from.id.."\n~Your #Message: "..msg.text.."\n~Your #Phone: +"..msg.from.phone.."\n~SuperGroup #Name: "..msg.to.print_name.."\n~SuperGroup #Id: "..msg.to.id..
 end
 
 return {
